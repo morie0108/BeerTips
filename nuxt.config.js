3602d5bf1bd67c07ad55b97795bf74d27a3a8ec3
Yohei <morizane.yo@gmail.com>
2021-12-18 23:25:40 +0900

    Change title

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,8 +10,8 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - FTips',
-    title: 'FTips',
+    titleTemplate: '%s - BeerTips',
+    title: 'BeerTips',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },

b719afad00c353e89fd27414c968736ad1df878e
Yohei <morizane.yo@gmail.com>
2021-12-31 11:10:57 +0900

    change favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,7 @@ export default {
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/pale-61.png' }]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css

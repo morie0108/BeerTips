f03b95f6949a15cfb9ca3ec91eefaf44e0aa25ec
Yohei <morizane.yo@gmail.com>
2022-10-03 08:31:33 +0900

    fadeUp

--- components/contents/bunnerComponent.vue
+++ components/contents/bunnerComponent.vue
@@ -1,62 +1,63 @@
 <template>
-  <v-row
-  >
-    <h2>クラフトビールを楽しむ Tips！</h2>
-    <v-col  cols="12" sm="8" md="4">
-      <v-card :width="width">
-        <nuxt-link to="/posts/about-craftbeer">
-          <v-img v-bind:src="require('@/assets/images/bunner_aboutBeer.png')"></v-img>
-        </nuxt-link>
-      </v-card>
-    </v-col>
-    <v-col  cols="12" sm="8" md="4">
-      <v-card :width="width">
-        <nuxt-link to="/tags/beerstyle">
-          <v-img v-bind:src="require('@/assets/images/bunner_beerType.png')"></v-img>
-        </nuxt-link>
-      </v-card>
-    </v-col>
-    <v-col  cols="12" sm="8" md="4">
-      <v-card :width="width">
-        <nuxt-link to="/categories/beer">
-          <v-img v-bind:src="require('@/assets/images/bunner_likeBeer.png')"></v-img>
-        </nuxt-link>
-      </v-card>
-    </v-col>
-  </v-row>
+  <fade-in-component>
+    <v-row>
+        <h2>クラフトビールを楽しむ Tips！</h2>
+            <v-col  cols="12" sm="8" md="4" style="margin:auto;">
+              <v-card :width="width">
+                <nuxt-link to="/posts/about-craftbeer">
+                  <v-img v-bind:src="require('@/assets/images/bunner_aboutBeer.png')"></v-img>
+                </nuxt-link>
+              </v-card>
+            </v-col>
 
+            <v-col  cols="12" sm="8" md="4" style="margin:auto;">
+              <v-card :width="width">
+                <nuxt-link to="/tags/beerstyle">
+                  <v-img v-bind:src="require('@/assets/images/bunner_beerType.png')"></v-img>
+                </nuxt-link>
+              </v-card>
+            </v-col>
+            <v-col  cols="12" sm="8" md="4" style="margin:auto;">
+              <v-card :width="width">
+                <nuxt-link to="/categories/beer">
+                  <v-img v-bind:src="require('@/assets/images/bunner_likeBeer.png')"></v-img>
+                </nuxt-link>
+              </v-card>
+            </v-col>
+
+        <!-- </div>
+    </div> -->
+    </v-row>
+  </fade-in-component>
 </template>
 
 <script>
+import fadeInComponent from '~/components/ui/fadeInComponent'
+
 
 export default {
-  // data () {
-  //   return {
-
-  //   }
-  // },
-  // computed: {
-  //   width () {
-  //     switch (this.$vuetify.breakpoint.name) {
-  //       case 'xs':
-  //         return 320
-  //       case 'sm':
-  //         return 500
-  //       case 'md':
-  //       case 'lg':
-  //       case 'xl':
-  //         return 280
-  //     }
-  //   },
-  // }
+  components:{
+    fadeInComponent
+  },
+  computed: {
+    width () {
+      switch (this.$vuetify.breakpoint.name) {
+        case 'xs':
+          return 320
+        case 'sm':
+          return 500
+        case 'md':
+        case 'lg':
+        case 'xl':
+          return 600
+      }
+    },
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-  // .v-toolbar__title{
-  //   margin: auto;
-  //   padding-right: 72px;
-  // }
+
   h2 {
     width: 100%;
     font-size: large;
@@ -65,15 +66,22 @@ export default {
   }
 
   .row {
-      justify-content: center;
-      margin: 0 auto;
+    justify-content: center;
+    margin: 0 auto;
+    height: 460px;
+    @media only screen and (min-width:600px) {
+      height: 640px;
+    }
+    @media only screen and (min-width:960px) {
+      height: 220px;
+    }
+    @media only screen and (min-width:1500px) {
+      height: 320px;
+    }
   }
 
-  .card {
+  .v-card ::v-deep {
       margin: auto;
   }
 
-
 </style>
-
-<style
---
+++ components/ui/fadeInComponent.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="fadeUp">
+      <slot v-show="visible"></slot>
+    </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        visible: false
+      };
+    },
+    created() {
+      if (process.browser) {
+        window.addEventListener("scroll", this.handleScroll);
+      }
+    },
+    destroyed() {
+      if (process.browser) {
+        window.removeEventListener("scroll", this.handleScroll);
+      }
+    },
+    methods: {
+      handleScroll() {
+        if (!this.visible) {
+          var top = this.$el.getBoundingClientRect().top;
+          this.visible = top < window.innerHeight + 100;
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+.fadeUp {
+  animation-name:fadeUpAnime;
+  animation-duration:0.5s;
+  animation-fill-mode:forwards;
+  opacity:0;
+}
+@keyframes fadeUpAnime{
+  from {
+    opacity: 0;
+	  transform: translateY(200px);
+  }
+
+  to {
+    opacity: 1;
+	  transform: translateY(0);
+  }
+}
+</style>
